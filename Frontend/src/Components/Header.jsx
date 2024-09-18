1000c16767f27c5600e1c3cadcae04c86500c8e8
Atribu <oguzkilinc.ant@gmail.com>
2024-09-18 12:18:40 +0300

    asd1

---
+++ Frontend/src/Components/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header>
+      <Link>
+        <img></img>
+      </Link>
+      <nav>
+        
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
