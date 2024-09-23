dd1ba1c05b26714559fb2bc34df0307b495d4dde
Atribu <oguzkilinc.ant@gmail.com>
2024-09-23 09:27:34 +0300

    pazartesi

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,22 +1,27 @@
 import React from 'react'
 import Header from "./Components/Header.jsx"
-
 import Header1 from './Components/Header1.jsx'
-gSection from './Components/BlogSection.jsx'
 import Limitation from './Components/Limitation.jsx'
 import MainCarousel from './Components/MainCarousel.jsx'
 import Footer from './Components/Footer.jsx'
+import Satis1 from "./Components/Satis1.jsx"
+import Satis2 from "./Components/Satis2.jsx"
+import BlogSection from "./Components/BlogSection.jsx"
+import TeklifForm from './Components/TeklifForm.jsx'
+
 
 const App = () => {
   return (
     <div>
-
-      <Header1 /> */}
+      <Header />
+      <Header1 />
       <MainCarousel/>
       <Limitation/>
       <Satis1/>
-      <Satis2/> 
-     <BlogSection/>
+      <Satis2/>
+      <BlogSection />
+      <Footer />
+      <TeklifForm />
     </div>
   )
 }
