a054df6723dd283c90152e5a413f2acf877df03e
EceOwl20 <ecebayramer20@gmail.com>
2024-09-30 10:42:22 +0300

    bread crumbs added

--- Frontend/src/blogPages/BpTasitmatikNedir.jsx
+++ Frontend/src/blogPages/BpTasitmatikNedir.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import BreadCrumbs from '../Components/BreadCrumbs'
 
 const BpTasitmatikNedir = () => {
   return (
-    <div>bpTasitmatikNedir</div>
+    <section>
+      <Link>MEVLANA PETROL</Link>
+      <h1>BP Taşıtmatik Nedir?</h1>
+      <Link>
+            <img />
+      </Link>
+      <Link>
+            <span></span>
+      </Link>
+      <BreadCrumbs />
+    </section>
   )
 }
 
