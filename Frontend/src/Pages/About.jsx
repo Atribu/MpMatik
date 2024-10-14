057f74255f91b99c2aff0355fb5088bfca97881d
Atribu <oguzkilinc.ant@gmail.com>
2024-10-14 12:43:56 +0300

    CssGuncelleme

--- Frontend/src/Pages/About.jsx
+++ Frontend/src/Pages/About.jsx
@@ -6,18 +6,27 @@ import Background from '../Components/Background'
 import AboutMain from "../Components/subPageComponents/AboutMain"
 import AboutGreenSection from '../Components/subPageComponents/AboutGreenSection'
 import Questions from '../Components/subPageComponents/Questions'
+import { Helmet } from 'react-helmet'
 
 const About = () => {
   return (
-    <div>
-      <AboutMain/>
-      <AboutGreenSection/>
-      <Questions/>
-      <BlogSection/>
-      <ContactForm2/>
-      <Background/>
-      <RoadSimulation/>
-    </div>
+    <>
+      <Helmet>
+        <title>Mevlana Petrol | Hakkımızda</title>
+        <meta name="description" content="Mptaşıtmatik olarak, Araç taşıt tanıma sistemlerinnde öncü bir kuruluşuz. Misyonumuz, müşterilerimize en kaliteli hizmetleri sunarak beklentilerini aşmaktır. Detaylar için web sitemizi ziyaret edin." />
+        <meta name='keywords' content='Mptaşıtmatik, Hakkımızda, Akaryakıt, Hizmet, Güvenli' />
+        <meta name="author" content="Mptaşıtmatik" />
+      </Helmet>
+      <div>
+        <AboutMain/>
+        <AboutGreenSection/>
+        <Questions/>
+        <BlogSection/>
+        <ContactForm2/>
+        <Background/>
+        <RoadSimulation/>
+      </div>
+    </>
   )
 }
 
