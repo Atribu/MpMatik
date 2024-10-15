7e7962bba4ed3faa94d9307016a981ebdab11bef
Atribu <oguzkilinc.ant@gmail.com>
2024-10-15 12:59:44 +0300

    KUGJH

--- api/index.js
+++ api/index.js
@@ -1,19 +1,19 @@
 // index.js
 import express from "express";
 import mongoose from "mongoose";
-import cors from "cors";  
 import dotenv from "dotenv";
-
 import loginRegister from "./routes/loginRegister.js";
 import blogRoute from "./routes/blog.js";
 import userRoute from "./routes/user.js";
 import pageRouter from "./routes/page.js";
-import odemeRouter from "./routes/odeme.js"; 
+
+import paymentRoutes from './payment.js'
+
 
 dotenv.config(); // .env dosyasını kullanabilmek için
 
 // MongoDB bağlantısı
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO)
   .then(() => {
     console.log("Bağlandı, sorun yok...");
   })
@@ -21,16 +21,18 @@ mongoose.connect(process.env.MONGO_URI)
     console.log(db_error);
   });
 
+
 const app = express();
 app.use(express.json());
-app.use(cors()); // CORS middleware'i kullanıldı
+
 
 // Route'ları ekleme
 app.use("/api/giris", loginRegister);
 app.use("/api/blog", blogRoute);
 app.use("/api/user", userRoute);
 app.use("/api/page", pageRouter);
-app.use("/api/odeme", odemeRouter); // Ödeme route'u burada kullanıldı
+
+app.use('/api/payment', paymentRoutes);
 
 // Hata middleware'i
 app.use((error, request, response, next) => {
